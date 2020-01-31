acda9e6c67c6d370bac82b1ce1e94df38cf07d95
nat-valerio <nat.valerio95@gmail.com>
2020-01-31 17:26:02 -0300

    Add mascaras e verificacao em js.

--- Site/CadastrarProdutos/js/mask-val.js
+++ Site/CadastrarProdutos/js/mask-val.js
@@ -2,26 +2,28 @@ $(document).ready(function () {
 
     //Máscaras
     $('.codigo').mask('0000000000', { reverse: true });
-    $('.dinheiro').mask('##.##0,00', { reverse: true });
+    $('.dinheiro').mask('000.000.000.000,00', { reverse: true });
 
     /*Validacao de campos
 
-        function () {
-            'use strict';
-            window.addEventListener('load', function () {
-                // Get the forms we want to add validation styles to
-                var forms = document.getElementsByClassName('needs-validation');
-                // Loop over them and prevent submission
-                var validation = Array.prototype.filter.call(forms, function (form) {
-                    form.addEventListener('submit', function (event) {
-                        if (form.checkValidity() === false) {
-                            event.preventDefault();
-                            event.stopPropagation();
-                        }
+    (function () {
+        'use strict';
+        window.addEventListener('load', function () {
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.getElementsByClassName('js_twbs_form');
+            // Loop over them and prevent submission
+            var validation = Array.prototype.filter.call(forms, function (form) {
+                form.addEventListener('submit', function () {
+                }, false);
+                $("#submitBtn").click(function (event) {
+                    if (form.checkValidity() === false) {
                         form.classList.add('was-validated');
-                    }, false);
+                        event.preventDefault();
+                        event.stopPropagation();
+                    }
                 });
-            }, false);
-        })();*/
+            });
+        }, false);
+    })();*/
 
 });
