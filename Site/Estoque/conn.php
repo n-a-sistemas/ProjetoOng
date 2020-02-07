529f03c9f60e7e80689c9cc111c4cbb60a685626
AndreFormici <andre.formici@users.noreply.github.com>
2020-02-07 17:32:06 -0300

    Relatórios
    07-02 Inicio da tabela encriptada pelo Yan

--- Site/Estoque/conn.php
+++ Site/Estoque/conn.php
@@ -10,3 +10,4 @@ $conn = new mysqli($server, $user, $pass, $database);
 if($conn->connect_error){
     die('Falha ao conectar' . $conn->connect_error);
 }
+?>
--- Site/Estoque/encode.php
+++ Site/Estoque/encode.php
@@ -10,21 +10,6 @@
         while($linha=$resultado->fetch_assoc()){
             array_push($produtos, $linha);            
         }
-        echo json_encode(utf8_string_array_encode($produtos));
+        echo json_encode($produtos);
     }
-
-    function utf8_string_array_encode(&$array){
-        $func = function(&$value,&$key){
-            if(is_string($value)){
-                $value = utf8_encode($value);
-            }
-            if(is_string($key)){
-                $key = utf8_encode($key);
-            }
-            if(is_array($value)){
-                utf8_string_array_encode($value);
-            }
-        };
-        array_walk($array,$func);
-        return $array;
-    }
+?>
