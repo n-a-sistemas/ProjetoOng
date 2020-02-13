e364e5ff705ba0e8d45f7788a33b68aafa177a8e
nat-valerio <nat.valerio95@gmail.com>
2020-02-13 17:29:03 -0300

    Add modal na tela de login

--- Site/CadastrarProdutos/js/mask-val.js
+++ Site/CadastrarProdutos/js/mask-val.js
@@ -2,35 +2,12 @@ $(document).ready(function () {
 
     //Máscaras
     $('.codigo').mask('0000000000', { reverse: true });
-    $('.quantidade').mask('0000', { reverse: true });
-    $('.dinheiro').mask('000.000.000.000,00', { reverse: true });
-
-    /*Validacao de campos
-
-    (function () {
-        'use strict';
-        window.addEventListener('load', function () {
-            // Fetch all the forms we want to apply custom Bootstrap validation styles to
-            var forms = document.getElementsByClassName('js_twbs_form');
-            // Loop over them and prevent submission
-            var validation = Array.prototype.filter.call(forms, function (form) {
-                form.addEventListener('submit', function () {
-                }, false);
-                $("#submitBtn").click(function (event) {
-                    if (form.checkValidity() === false) {
-                        form.classList.add('was-validated');
-                        event.preventDefault();
-                        event.stopPropagation();
-                    }
-                });
-            });
-        }, false);
-    })();*/
+    $('.dinheiro').mask('000.000,00', { reverse: true });
 
     //Inserindo o nome do arquivo no campo de imagem
     $(".custom-file-input").on("change", function() {
         var fileName = $(this).val().split("\\").pop();
         $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
-      });
+    });
 
 });
