b0e5776d0f26fe026e3ce99b34d44b2e12b42cb1
YAN ANTONIO TACONELLI <yan.ataconelli@senacsp.edu.br>
2020-02-17 14:56:40 -0300

    Quase finalizando 1.3

--- Site/CadastrarUsuarios/form.php
+++ Site/CadastrarUsuarios/form.php
@@ -7,34 +7,42 @@
 </head>
 <body>
     <div>
-        <form action="../Login/login.php" method="POST">
+        <form action="insert.php" method="POST">
             <div>
                 <label for="nome">Nome Completo: </label>
                 <input type="text" name="nome" id="nome">
             </div>
             <div>
-                <label for="email">Email: </label>
+                <label for="email">E-mail: </label>
                 <input type="email" name="email" id="email">
+            </div>
+            <div>
+                <label for="confirmaemail">Confirme seu e-mail: </label>
+                <input type="email" name="confirmaemail" id="confirmaemail">
             </div>
             <div>
                 <label for="senha">Senha: </label>
                 <input type="password" name="senha" id="senha">
             </div>
             <div>
+                <label for="confirmasenha">Confirme sua senha: </label>
+                <input type="password" name="confirmasenha" id="confirmasenha">
+            </div>
+            <div>
                 <fieldset>
-                <legend>Tipo de usuário: </legend>
-                    <div>
-                        <input type="radio" name="adm" id="adm">
-                        <label for="adm">Administrador</label>
-                    </div>
-                    <div>
-                        <input type="radio" name="comum" id="comum">
-                        <label for="comum">Usuário comum</label>
-                    </div>
+                    <legend>Tipo de usuário: </legend>
+                        <div>
+                            <input type="radio" name="user" id="adm" value="1">
+                            <label for="adm">Administrador</label>
+                        </div>
+                        <div>
+                            <input type="radio" name="user" id="comum" value="2">
+                            <label for="comum">Usuário comum</label>
+                        </div>
                 </fieldset>
-                <div>
-                    <input type="submit" value="Cadastrar">
-                </div>
+            </div>
+            <div>
+                <input type="submit" value="Cadastrar">
             </div>
         </form>
     </div>
