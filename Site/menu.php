7978b6e510a8c894c08d032d84cff47507160e2f
AndreFormici <andre.formici@users.noreply.github.com>
2020-02-21 17:00:42 -0300

    Mudança do menu
    Inicio das tentativas de fazer com que o menu também indique em que página você esta.

--- Site/menu.php
+++ Site/menu.php
@@ -40,7 +40,7 @@
                 </li>
 
                 <li  class="nav-item">
-                    <a class="nav-link" href="../MeuPerfil/perfil.php">Meu perfil</a>
+                    <a class="nav-link patual" href="../MeuPerfil/perfil.php">Meu perfil</a>
                 </li>
 
                 <li class="nav-item">
