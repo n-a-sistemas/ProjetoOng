ad55875906786ac7ed88bdd92321177197b6d695
nat-valerio <nat.valerio95@gmail.com>
2020-03-08 18:47:12 -0300

    Inserindo validação e pequenos ajustes

--- Site/relatoriofinanceiro/index.php
+++ Site/relatoriofinanceiro/index.php
@@ -27,16 +27,17 @@ if($result->num_rows > 0){
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"/>
-    <link rel="stylesheet" href="css/adminlte.min.css">
+    <link rel="stylesheet" href="css/adminlte.min.css"/>
     <link rel="stylesheet" href="../menu.css"/>
+    <link rel="stylesheet" href="estilo.css"/>
     <title>Relatório Financeiro</title>
 </head>
 <body>  
-    <?php include "../menu.php"?>      
+    <?php include "../menu.php"; ?>      
     <div class="container">
         <div>
             <div class="container text-center col primary">
