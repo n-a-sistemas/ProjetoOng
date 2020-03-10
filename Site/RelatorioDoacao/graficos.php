ae379bb700ed2596df69e675d706325d44c44b60
AndreFormici <andre.formici@users.noreply.github.com>
2020-03-10 16:40:32 -0300

    Corrigindo erros
    merge apagou dados.

--- Site/RelatorioDoacao/graficos.php
+++ Site/RelatorioDoacao/graficos.php
@@ -5,14 +5,14 @@
         <div class="row">
             <div class="d-flex align-items-stretch">
                 <!-- Grafico 1 -->
-                <div class="col-7">
+                <div class="col-12">
                     <div class="card card-primary d-flex d-inline">
                         <div class="card-header">
                             <h3 class="card-title">Doações</h3>
                         </div>
                         <div class="card-body">
                             <div class="chart">
-                                <canvas id="bargrafico"
+                                <canvas id="barChart"
                                     style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
                             </div>
                         </div> <!-- /.card-body -->
